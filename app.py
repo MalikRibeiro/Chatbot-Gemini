21ff4759583c679e53a6eb5997c0d5c5d062ed64
Malik Ribeiro Mourad <malik.mourad@grupoelectra.com.br>
2025-06-27 10:41:06 -0300

    Nova funcionalidade gerenciar historico

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from time import sleep
 from helper import carrega, salva
 from selecionar_persona import personas, selecionar_persona
+from gerenciar_historico import remover_mensagens_mais_antigas
 
 load_dotenv()
 
@@ -17,15 +18,10 @@ app.secret_key = 'alura'
 
 contexto = carrega("dados\musimart.txt")
 
-def bot(prompt):
-    maximo_tentativas = 1
-    repeticao = 0
+def criar_chatbot():
+    personalidade = "neutro"
 
-    while True:
-        try:
-            personalidade = personas[selecionar_persona(prompt)]
-            
-            prompt_do_sistema = f"""
+    prompt_do_sistema = f"""
             # PERSONA
             
             Você é um chatbot de atendimento a clientes de um e-commerce. 
@@ -38,21 +34,52 @@ def bot(prompt):
             
             # PERSONALIDADE
             {personalidade}
+
+            # Histórico
+            Acesse sempre o histórico de mensagens, e recupere informações ditas anteriormente.
+            """
+    
+    configuracao_modelo = {
+        "temperature" : 0.1,
+        "max_output_tokens" : 8192
+    }
+
+    llm = genai.GenerativeModel(
+        model_name=MODELO_ESCOLHIDO,
+        system_instruction=prompt_do_sistema,
+        generation_config=configuracao_modelo
+    )
+
+    chatbot = llm.start_chat(history=[])
+
+    return chatbot
+
+chatbot = criar_chatbot()
+
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            personalidade = personas[selecionar_persona(prompt)]
+            mensagem_usuario = f"""
+            Considere esta personalidade para responder a mensagem:
+            {personalidade}
+        
+            Responda a seguinte mensagem, sempre lembrando do histórico:
+            {prompt}
             """
 
-            configuracao_modelo = {
-                "temperature" : 0.1,
-                "max_output_tokens" : 8192
-            }
+            resposta = chatbot.send_message(mensagem_usuario)
 
-            llm = genai.GenerativeModel(
-                model_name=MODELO_ESCOLHIDO,
-                system_instruction=prompt_do_sistema,
-                generation_config=configuracao_modelo
-            )
+            if len(chatbot.history) > 4:
+                chatbot.history = remover_mensagens_mais_antigas(chatbot.history)
 
-            resposta = llm.generate_content(prompt)
+            print(f"Quantidade: {len(chatbot.history)}\n{chatbot.history}")
+            
             return resposta.text
+
         except Exception as erro:
             repeticao += 1
             if repeticao >= maximo_tentativas:
